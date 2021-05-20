242b90fa4e747b8424ef1a075057e15767e10b49
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-05-20 16:29:21 +0200

    cleanded up

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,366 +1,266 @@
-/**
- * BlackJack Module
- *
- * Depends on Deck and Players Modules and defines the rules and logic of the blackjack game
- */
-var blackjack = (function () {
-  // index of the active player at a given moment
-  var currentPlayer = 0;
+document.getElementById("testiframe").addEventListener("load", loaded);
 
-  /**
-   * Initializes the game
-   *
-   * Starts the deck
-   * Starts the players and adds a new 'human' typed player
-   * Sets the Dom environment for the game
-   * Deals the cards to both players
-   * And sets the first active player to be the 'human'
-   */
-  var init = function () {
-    reset();
-    deck.init();
-    players.init();
-    players.newPlayer('you');
-
-    createPlayersUI();
-
-    dealCards();
-
-    currentPlayer = players.players().length - 1;
-
-    updateActivePlayer(players.players()[currentPlayer]);
-  };
-
-  /**
-   * Clears the game environment
-   */
-  var reset = function () {
-    var div_status = document.getElementById('status');
-    div_status.classList.remove('active');
-
-    var p_status_text = document.getElementById('status-text');
-    p_status_text.innerHTML = '';
-
-    document.getElementById('players').innerHTML = '';
-    document.getElementById('house').innerHTML = '';
-  };
-
-  /**
-   * Initial deal
-   *
-   * gives 2 cards to human players and 1 to auto players
-   */
-  var dealCards = function () {
-    players.players().map((player) => {
-      switch (player.type) {
-        case 'human':
-          while (player.hand.length < 2) {
-            hit(player, true);
-          }
-          break;
-        case 'auto':
-        default:
-          hit(player, true);
-          break;
-      }
-    });
-  };
-
-  /**
-   * handles player's points according to the respective hand
-   *
-   * @param {object} player
-   */
-  var handlePoints = function (player) {
-    // to handle aces last
-    var orderedCards = [];
-    var points = 0;
-
-    if (player.hand.length > 0) {
-      player.hand.map((card) => {
-        if (card.Value === 'A') {
-          orderedCards.push(card);
-        } else {
-          orderedCards.unshift(card);
-        }
-      });
-
-      orderedCards.map((card) => {
-        points += cardWeight(card, points);
-      });
-
-      player.points = points;
-    }
-    updatePlayerPoints(player);
-
-    checkWinners(player);
-  };
-
-  /**
-   * returns the points referent to a given card value
-   * if it's an ace checks if should be 11 or 1
-   *
-   * @param {object} card
-   * @param {number} currentPoints
-   */
-  var cardWeight = function (card, currentPoints) {
-    var faces = ['J', 'Q', 'K'];
-
-    var weight = parseInt(card.Value);
-
-    if (faces.indexOf(card.Value) > -1) {
-      weight = 10;
-    }
-
-    if (card.Value === 'A') {
-      weight = currentPoints + 11 > 21 ? 1 : 11;
-    }
-    return weight;
-  };
-
-  /**
-   * Takes a card from the deck and place it on player's hand
-   * If its dealing phase ignore autoplay
-   *
-   * @param {object} player
-   * @param {bool} dealing
-   */
-  var hit = function (player, dealing = false) {
-    var card = deck.deal();
-    player.hand.push(card);
-    handlePoints(player);
-    renderCard(card, player);
-    updateDeck();
-
-    if (!dealing && player.type != 'human') {
-      autoPlay(player);
-    }
-  };
-
-  /**
-   * Process what happens after player decides to not take more cards
-   *
-   * @param {object} player
-   */
-  var stay = function (player) {
-    currentPlayer--;
-    if (currentPlayer >= 0) {
-      updateActivePlayer(players.players()[currentPlayer]);
-
-      if (players.players()[currentPlayer].type != 'human') {
-        autoPlay(players.players()[currentPlayer]);
-      }
-    } else {
-      endGame();
-    }
-  };
-
-  /**
-   * Makes the decisions for a 'auto' player based on his and the other players points
-   * Uses a timeout just for visualisation effect on the Dom
-   *
-   * @param {object} player
-   */
-  var autoPlay = function (player) {
-    var highestScore = 0;
-
-    players.players().map((p) => {
-      if (p.id != player.id && p.points <= 21 && p.points > highestScore) {
-        highestScore = p.points;
-      }
-    });
-
-    window.setTimeout(() => {
-      // if points are inferior to other players have to hit
-      if (player.points <= highestScore) {
-        hit(player);
-      } else {
-        stay(player);
-      }
+function loaded() {
+    setTimeout(function() {
+        document.getElementById("loader").style.display = "none";
     }, 1000);
-  };
+    
+}
 
-  /**
-   * Checks who's the winner and creates a status on the DOM to display it
-   */
-  var endGame = function () {
-    var highestScore = 0;
-    var winner = {};
-
-    players.players().map((p) => {
-      if (p.points <= 21 && p.points > highestScore) {
-        highestScore = p.points;
-        winner = p;
-      }
-    });
-
-    updateStatus(winner.name + ' wins!', winner.type == 'human');
-  };
-
-  /**
-   * Checks if a player has blackjack or busts, defining winners before the expected end of the game
-   *
-   * @param {object} player
-   */
-  var checkWinners = function (player) {
-    if (player.points == 21) {
-      updateStatus(player.name + ' wins!', true);
-    } else if (player.points > 21) {
-      updateStatus(player.name + ' busted!', false);
-      stay(player);
-    }
-  };
-
-  /**
-   * Creates and sets the elements of the DOM for the existing players
-   */
-  var createPlayersUI = function () {
-    document.getElementById('players').innerHTML = '';
-    document.getElementById('house').innerHTML = '';
-
-    players.players().map((player) => {
-      var div_player = document.createElement('div');
-      var div_playerid = document.createElement('div');
-      var div_hand = document.createElement('div');
-      var span_points = document.createElement('span');
-      var div_actions = document.createElement('div');
-
-      span_points.className = 'points';
-      span_points.id = 'points_' + player.id;
-      div_player.id = 'player_' + player.id;
-      div_player.className = 'player';
-      div_hand.id = 'hand_' + player.id;
-      div_hand.className = 'hand';
-      div_actions.id = 'actions_' + player.id;
-
-      div_playerid.innerHTML = player.name + ': ';
-      div_playerid.appendChild(span_points);
-      div_player.appendChild(div_playerid);
-      div_player.appendChild(div_hand);
-      div_player.appendChild(div_actions);
-
-      switch (player.type) {
-        case 'human':
-          document.getElementById('players').appendChild(div_player);
-          break;
-        case 'auto':
-        default:
-          document.getElementById('house').appendChild(div_player);
-          break;
-      }
-    });
-  };
-
-  /**
-   * Render a card on the DOM of the correct place for a given player's hand
-   *
-   * @param {object} card
-   * @param {object} player
-   */
-  var renderCard = function (card, player) {
-    var hand = document.getElementById('hand_' + player.id);
-    hand.appendChild(deck.getCardUI(card));
-  };
-
-  /**
-   * Updates the current number of cards in the deck's DOM element
-   */
-  var updateDeck = function () {
-    document.getElementById('deckcount').innerHTML = deck.deck().length;
-  };
-
-  /**
-   * Changes who's the current active player in the DOM
-   *
-   * @param {object} activePlayer
-   */
-  var updateActivePlayer = function (activePlayer) {
-    players.players().map((player) => {
-      if (player.id === activePlayer.id) {
-        document.getElementById('player_' + player.id).classList.add('active');
-
-        if (player.type === 'human') {
-          var div_buttons = document.createElement('div');
-          var btn_hit = document.createElement('button');
-          var btn_stay = document.createElement('button');
-          btn_hit.className = 'btn';
-          btn_stay.className = 'btn';
-
-          btn_hit.innerHTML = 'Hit me!';
-          btn_hit.addEventListener(
-            'click',
-            function () {
-              hit(player);
-            },
-            false
-          );
-
-          btn_stay.innerHTML = 'Stay';
-          btn_stay.addEventListener(
-            'click',
-            function () {
-              stay(player);
-            },
-            false
-          );
-
-          div_buttons.appendChild(btn_hit);
-          div_buttons.appendChild(btn_stay);
-
-          document
-            .getElementById('actions_' + player.id)
-            .appendChild(div_buttons);
-        }
-      } else {
-        document
-          .getElementById('player_' + player.id)
-          .classList.remove('active');
-        document.getElementById('actions_' + player.id).innerHTML = '';
-      }
-    });
-    var div_status = document.getElementById('status');
-    div_status.classList.remove('active');
-  };
-
-  /**
-   * Updates the player points value in the DOM
-   *
-   * @param {object} player
-   */
-  var updatePlayerPoints = function (player) {
-    document.getElementById('points_' + player.id).innerHTML = player.points;
-  };
-
-  /**
-   * Show the status hidden element with given text
-   * element should have a button to restart the game
-   *
-   * @param {string} text
-   * @param {bool} win
-   */
-  var updateStatus = function (text, win) {
-    var div_status = document.getElementById('status');
-    div_status.classList.add('active');
-
-    var div_status_content = document.getElementById('status-content');
-    if (win) {
-      div_status_content.classList.remove('red');
-    } else {
-      div_status_content.classList.add('red');
-    }
-
-    var p_status_text = document.getElementById('status-text');
-    p_status_text.innerHTML = text;
-  };
-
-  return {
-    init: init,
-    reset: reset,
-  };
-})();
-
-document.onreadystatechange = function () {
-  if (document.readyState == 'interactive') {
-    blackjack.init();
-  }
+let player = {
+  cards: [],
+  total: 0,
+  bank: 2500,
 };
+let dealer = {
+  cards: [],
+  total: 0,
+};
+let drawnCards = [];
+let pot = 0;
+
+function deal() {
+  player.cards = [];
+  dealer.cards = [];
+  player.total = 0;
+  dealer.total = 0;
+  drawnCards = [];
+  document.getElementById('winner').innerHTML = '';
+  let playerCards = document.getElementById('player-cards');
+  while (playerCards.firstChild) {
+    playerCards.removeChild(playerCards.firstChild);
+  }
+  let dealerCards = document.getElementById('dealer-cards');
+  while (dealerCards.firstChild) {
+    dealerCards.removeChild(dealerCards.firstChild);
+  }
+
+  draw(player);
+  draw(player);
+  draw(dealer);
+  play();
+  document.getElementById('deal').style.visibility = 'hidden';
+  document.getElementById('hit').style.visibility = 'visible';
+  document.getElementById('stand').style.visibility = 'visible';
+  document.getElementById('place').style.visibility = 'hidden';
+}
+
+function draw(person) {
+  const number = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+  const suit = ['♠', '♦', '♥', '♣'];
+  const randomSuit = Math.floor(Math.random() * suit.length);
+  const random = Math.floor(Math.random() * number.length);
+  console.log(randomSuit);
+  let newCard = number[random] + ' ' + suit[randomSuit];
+
+  drawnCards.forEach((card) => {
+    if (card == newCard) {
+      newCard = '';
+      draw(person);
+    }
+  });
+  if (newCard != '') {
+    if (
+      number[random] == 'K' ||
+      number[random] == 'Q' ||
+      number[random] == 'J'
+    ) {
+      person.total = person.total + 10;
+    } else if (number[random] == 'A' && person.total < 11) {
+      person.total = person.total + 11;
+    } else if (number[random] == 'A' && person.total > 10) {
+      person.total = person.total + 1;
+    } else {
+      person.total = person.total + number[random];
+    }
+
+    drawnCards.push(newCard);
+    person.cards.push(newCard);
+  }
+}
+function play() {
+  player.cards.forEach((card) => {
+    let div = document.createElement('div');
+    div.className = 'card slide';
+    div.innerHTML = card;
+
+    let value = document.createElement('p');
+    value.innerHTML = card;
+    value.className = 'card-suit';
+    div.appendChild(value);
+
+    document.getElementById('player-cards').appendChild(div);
+  });
+  dealer.cards.forEach((card) => {
+    let div = document.createElement('div');
+    div.className = 'card slide';
+    div.innerHTML = card;
+    let value = document.createElement('p');
+    value.innerHTML = card;
+    value.className = 'card-suit';
+    div.appendChild(value);
+    document.getElementById('dealer-cards').appendChild(div);
+  });
+  let div = document.createElement('div');
+  div.className = 'card slide';
+  div.id = 'flip';
+  document.getElementById('dealer-cards').appendChild(div);
+  document.getElementById('coins').style.visibility = 'hidden';
+  total();
+  if (player.total == 21) {
+    stand();
+  }
+}
+function hit() {
+  draw(player);
+  let newCard = document.createElement('div');
+  newCard.className = 'card slide';
+  console.log(player.cards);
+
+  newCard.innerHTML = player.cards[player.cards.length - 1];
+
+  console.log(newCard.innerHTML);
+
+  let value = document.createElement('p');
+  value.innerHTML = newCard.innerHTML;
+  value.className = 'card-suit';
+
+  newCard.appendChild(value);
+
+  document.getElementById('player-cards').appendChild(newCard);
+  total();
+  if (player.total > 21) {
+    draw(dealer);
+    total();
+    winner(dealer);
+    document.getElementById('flip').style = 'display:none';
+    let newCard = document.createElement('div');
+    newCard.className = 'card slide';
+    newCard.innerHTML = dealer.cards[dealer.cards.length - 1];
+
+    let value = document.createElement('p');
+    value.innerHTML = newCard.innerHTML;
+    value.className = 'card-suit';
+
+    newCard.appendChild(value);
+
+    document.getElementById('dealer-cards').appendChild(newCard);
+  }
+}
+function total() {
+  document.getElementById('player-total').innerHTML =
+    ' Your Total: ' + player.total;
+  document.getElementById('dealer-total').innerHTML =
+    ' Dealer Total: ' + dealer.total;
+}
+
+function stand() {
+  draw(dealer);
+  document.getElementById('flip').style = 'display:none';
+  var card4 = document.createElement('div');
+  card4.className = 'card slide mx-1';
+  card4.innerHTML = dealer.cards[dealer.cards.length - 1];
+  let value = document.createElement('p');
+  value.innerHTML = card4.innerHTML;
+  value.className = 'card-suit';
+
+  card4.appendChild(value);
+  document.getElementById('dealer-cards').appendChild(card4);
+  total();
+  while (player.total > dealer.total && dealer.total < 21) {
+    stand();
+  }
+
+  if (dealer.total > 21) {
+    winner(player);
+  } else if (player.total == 21 && dealer.total != 21) {
+    winner(player);
+  } else if (dealer.total == 21 && player.total != 21) {
+    winner(dealer);
+  } else if (dealer.total == player.total) {
+    winner(draw);
+  } else {
+    if (player.total < dealer.total) {
+      winner(dealer);
+    } else {
+      winner(player);
+    }
+  }
+}
+function bet(amount = 'all') {
+  if (amount == 'all') {
+    amount = player.bank;
+  }
+  if (amount <= player.bank) {
+    pot = pot + amount;
+    player.bank = player.bank - amount;
+    document.getElementById('bet').innerHTML = 'Bet: $' + pot;
+    document.getElementById('player-bank').innerHTML =
+      'Your Bank: $' + player.bank;
+    newPot();
+  }
+}
+function winner(person) {
+  switch (person) {
+    case dealer:
+      pot = 0;
+      document.getElementById('bet').innerHTML = 'Bet: $' + pot;
+      document.getElementById('player-bank').innerHTML =
+        'Your bank: $' + player.bank;
+      document.getElementById('winner').innerHTML = 'Dealer Wins!!!!';
+      document.getElementById('place').style.visibility = 'visible';
+      document.getElementById('coins').style.visibility = 'visible';
+      newPot();
+      break;
+
+    case player:
+      player.bank = player.bank + pot * 2;
+      pot = 0;
+      document.getElementById('bet').innerHTML = 'Bet: $' + pot;
+      document.getElementById('player-bank').innerHTML =
+        'Your bank: $' + player.bank;
+      document.getElementById('winner').innerHTML = 'You Win!!!!';
+
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+      });
+      document.getElementById('place').style.visibility = 'visible';
+      document.getElementById('coins').style.visibility = 'visible';
+
+      newPot();
+      break;
+
+    case draw:
+      document.getElementById('bet').innerHTML = 'Bet: $' + pot;
+      document.getElementById('player-bank').innerHTML =
+        'Your bank: $' + player.bank;
+      document.getElementById('winner').innerHTML = 'Draw!!!!';
+      document.getElementById('place').style.visibility = 'visible';
+      document.getElementById('hit').style.visibility = 'hidden';
+      document.getElementById('stand').style.visibility = 'hidden';
+      document.getElementById('deal').style.visibility = 'visible';
+      document.getElementById('coins').style.visibility = 'visible';
+      break;
+  }
+}
+
+function newPot() {
+  if (pot == 0) {
+    document.getElementById('hit').style.visibility = 'hidden';
+    document.getElementById('stand').style.visibility = 'hidden';
+    document.getElementById('deal').style.visibility = 'hidden';
+  } else {
+    document.getElementById('deal').style.visibility = 'visible';
+  }
+}
+
+function newGame() {
+  document.getElementById('winner').innerHTML = '';
+  document.getElementById('place').style.visibility = 'visible';
+  document.getElementById('player-bank').innerHTML =
+    'Your bank: $' + player.bank;
+  document.getElementById('bet').innerHTML = 'Bet: $' + pot;
+  newPot();
+}
