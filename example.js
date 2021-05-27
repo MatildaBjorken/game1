ce9a0558c01f9126f1e7a4796ac7fc1708fe64b0
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-05-27 18:47:17 +0200

    max width

---
+++ example.js
@@ -0,0 +1,8 @@
+
+
+let Author = article.AuthorName();
+// ...more code here...
+console.log("The author of the article is: " + Author);
+
+// ...more code here...
+console.log("The author of the book is: " + Book.AuthorName());
